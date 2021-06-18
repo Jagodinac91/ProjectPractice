932193cdda5550083cffbc8c5ccd8262383f5f00
Jagodinac91 <74147076+Jagodinac91@users.noreply.github.com>
2021-06-18 11:45:26 -0700

    Initial commit

---
+++ src/test/java/com/someCompanyName/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.someCompanyName.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        //plugin = {}
+        features = "src/test/resources/features",
+        glue = "com/someCompanyName/step_definitions",
+        dryRun = false,
+        tags = ""
+)
+
+
+public class CukesRunner {
+}
